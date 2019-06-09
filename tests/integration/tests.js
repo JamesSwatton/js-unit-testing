35503326531338ff081841534622b08185f2fbc1
James Swatton <jamessswatton@gmail.com>
2019-06-09 22:57:39 +0100

    add display negative numbers correctly test to UI integration tests

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -52,4 +52,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('7')
   })
 
+  it('should display negative numbers correctly', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+  })
 });
