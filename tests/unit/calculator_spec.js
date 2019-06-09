2ea12bceb3a758378c452b72b840a3a3a49b32ae
James Swatton <jamessswatton@gmail.com>
2019-06-09 21:47:33 +0100

    add add / subtract / multiply tests to calculator spec

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,4 +11,25 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('can add two numbers together', function () {
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 5)
+  })
+
+  it('can subtract two numbers together', function () {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 3)
+  })
+
+  it('can multiply two numbers together', function () {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 15)
+  })
+
 });
