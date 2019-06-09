fcd8ad895f4d45c67a68864cecf0dfa4ea6f4e4d
James Swatton <jamessswatton@gmail.com>
2019-06-09 23:01:10 +0100

    add display decimal numbers correctly test to UI integration tests

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -60,4 +60,14 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('-3')
   })
+
+  it('should display decimal numbers correctly', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5')
+
+  })
 });
